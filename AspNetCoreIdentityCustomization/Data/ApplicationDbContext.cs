e6085f4bcdf68562430a72983d55cf351b43d307
TathagatGitHub <tathagat.dwivedi@gmail.com>
2019-02-02 20:23:25 -0800

    Add project files.

---
+++ AspNetCoreIdentityCustomization/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AspNetCoreIdentityCustomization.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentityCustomization.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+    }
+}
