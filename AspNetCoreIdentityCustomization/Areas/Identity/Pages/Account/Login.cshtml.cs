9bd5b308c133eb8a3aff23a0d7c4662cdcd59514
Tathagat Dwivedi <tathagat.dwivedi@OceanMediainc.com>
2019-12-19 21:23:31 -0800

    Core-Data1

--- AspNetCoreIdentityCustomization/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AspNetCoreIdentityCustomization/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -77,7 +77,7 @@ namespace AspNetCoreIdentityCustomization.Areas.Identity.Pages.Account
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User logged in.");
+                    _logger.LogInformation("User "+ Input.Email + " logged in.");
                     return LocalRedirect(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
@@ -86,7 +86,7 @@ namespace AspNetCoreIdentityCustomization.Areas.Identity.Pages.Account
                 }
                 if (result.IsLockedOut)
                 {
-                    _logger.LogWarning("User account locked out.");
+                    _logger.LogWarning("User " + Input.Email + " locked out.");
                     return RedirectToPage("./Lockout");
                 }
                 else
